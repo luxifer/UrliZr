2828db63681ada201e73af3ecd74e118ec3f48f4
Florent Viel <luxifer666@gmail.com>
2011-09-25 14:42:46 +0200

    traceback

--- api/views.py
+++ api/views.py
@@ -19,10 +19,9 @@ from django.shortcuts import render_to_response,redirect
 from UrliZr.front.models import Urliz
 from UrliZr.front.forms import UrlizForm
 from django.http import HttpResponseBadRequest
-from urllib import quote
 
 def translate(request, method, url):
-  form = UrlizForm({'url': quote(url)})
+  form = UrlizForm({'url': url})
   
   if form.is_valid():
     if Urliz.objects.filter(url__exact=form.cleaned_data['url']).count() == 0:
