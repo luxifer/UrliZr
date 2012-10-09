64724554518daac82fb3b34d0512273fa935fc27
Florent Viel <fviel@wanadev.fr>
2012-10-09 15:04:53 +0200

    correction pep8

--- front/functions.py
+++ front/functions.py
@@ -18,6 +18,7 @@ along with UrliZr.  If not, see <http://www.gnu.org/licenses/>.
 import string
 from random import choice
 
+
 def genUid():
   chars = string.letters + string.digits
   return u''.join(choice(chars) for i in range(8))
