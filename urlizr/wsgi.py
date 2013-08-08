43ae9d910c34c0879b77b794c1ceacf267113e02
Florent Viel <fviel@wanadev.fr>
2013-08-08 11:28:22 +0200

    Fis: wsgi

--- urlizr/wsgi.py
+++ urlizr/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urlizr.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
